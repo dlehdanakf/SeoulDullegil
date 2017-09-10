37eb8a84e634bc9c511919886ca810b213a8f35c
LEE Dong Hyun <dlehdanakf@konkuk.ac.kr>
2017-09-10 23:11:06 +0900

    코스 리스트 JS 객체로 저장

---
+++ App/pages/datasets/course.list.js
@@ -0,0 +1,66 @@
+export default [
+    {
+        LOCATION: '노원구, 도봉구',
+        DISTANCE: '14.3km',
+        WALK_TIME: '6시간 30분',
+        COURSE_NO: '1',
+        COURSE_LEVEL: '3',
+        COURSE_NM: '수락·불암산코스'
+    },
+    {
+        LOCATION: '광진구, 중랑구',
+        DISTANCE: '12.6km',
+        WALK_TIME: '5시간 10분',
+        COURSE_NO: '2',
+        COURSE_LEVEL: '2',
+        COURSE_NM: '용마·아차산코스'
+    },
+    {
+        LOCATION: '강동구, 송파구',
+        DISTANCE: '26.1km',
+        WALK_TIME: '9시간',
+        COURSE_NO: '3',
+        COURSE_LEVEL: '1',
+        COURSE_NM: '고덕·일자산코스'
+    },
+    {
+        LOCATION: '강남구, 서초구',
+        DISTANCE: '17.9km',
+        WALK_TIME: '8시간',
+        COURSE_NO: '4',
+        COURSE_LEVEL: '2',
+        COURSE_NM: '대모·우면산코스'
+    },
+    {
+        LOCATION: '관악구, 금천구',
+        DISTANCE: '12.7km',
+        WALK_TIME: '5시간 50분',
+        COURSE_NO: '5',
+        COURSE_LEVEL: '2',
+        COURSE_NM: '관악산코스'
+    },
+    {
+        LOCATION: '강서구, 구로구, 금천구',
+        DISTANCE: '18km',
+        WALK_TIME: '4시간 30분',
+        COURSE_NO: '6',
+        COURSE_LEVEL: '1',
+        COURSE_NM: '안양천코스'
+    },
+    {
+        LOCATION: '마포구, 은평구',
+        DISTANCE: '16.6km',
+        WALK_TIME: '6시간 10분',
+        COURSE_NO: '7',
+        COURSE_LEVEL: '2',
+        COURSE_NM: '봉산·앵봉산코스'
+    },
+    {
+        LOCATION: '강북구, 도봉구, 성북구, 은평구, 종로구',
+        DISTANCE: '34.5km',
+        WALK_TIME: '17시간',
+        COURSE_NO: '8',
+        COURSE_LEVEL: '2',
+        COURSE_NM: '북한산코스'
+    }
+];
