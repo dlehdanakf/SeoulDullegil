0c6b1ced8b25af2d286df9cf8632d05e50c04431
Kuril951 <viodle238@naver.com>
2017-09-22 14:26:35 +0900

    merge branch

--- App/Navigators.js
+++ App/Navigators.js
@@ -14,13 +14,8 @@ import Tracking from './pages/Tracking';
 const MyNavigator = () => (
   <Router>
       <Scene key="root" headerMode="none">
-<<<<<<< HEAD
-=======
-          <Scene key="tracking" component={Tracking} />
           <Scene key="course_map" component={CourseMap} />
->>>>>>> twDevelop
           <Scene key="course_info" component={CourseInfo} />
-          <Scene key="course_map" component={CourseMap} />
       </Scene>
   </Router>
 );
