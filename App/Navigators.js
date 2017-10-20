63f03fec6111fdc874a169cf6bb82eb29ac62022
Kuril951 <viodle238@naver.com>
2017-10-20 20:35:57 +0900

    add template

--- App/Navigators.js
+++ App/Navigators.js
@@ -6,15 +6,19 @@ import {
 import CourseInfo from './pages/CourseInfo.page';
 import CourseMap from './pages/CourseMap.page';
 import Tracking from './pages/Tracking';
+import Home from './pages/Home.page'
+import CourseList from './pages/CourseList.page'
 
 const MyNavigator = () => (
-  <Router>
-      <Scene key="root" headerMode="none">
-          <Scene key="tracking" component={Tracking} />
-          <Scene key="course_map" component={CourseMap} />
-          <Scene key="course_info" component={CourseInfo} />
-      </Scene>
-  </Router>
+    <Router>
+        <Scene key="root" headerMode="none">
+            <Scene key="course_list" component={CourseList} />
+                <Scene key="home" component={Home} />
+                <Scene key="tracking" component={Tracking} />
+                <Scene key="course_map" component={CourseMap} />
+                <Scene key="course_info" component={CourseInfo} />
+        </Scene>
+    </Router>
 );
 
 export default MyNavigator;
