c9a03408481e1e560264d0c80a15dbc5d4c1ca26
LEE Dong Hyun <dlehdanakf@konkuk.ac.kr>
2017-10-22 23:19:17 +0900

    둘레길 소개 페이지 제작

--- App/Navigators.js
+++ App/Navigators.js
@@ -8,12 +8,14 @@ import CourseMap from './pages/CourseMap.page';
 import Tracking from './pages/Tracking';
 import Home from './pages/Home';
 import Notice from './pages/Notice.page';
-import CourseList from './pages/CourseList.page'
+import CourseList from './pages/CourseList.page';
+import Guide from './pages/Guide.page';
 import Main from './pages/Main.page';
 
 const MyNavigator = () => (
     <Router>
         <Scene key="root" headerMode="none">
+            <Scene key="guide" component={Guide} />
             <Scene key="notice" component={Notice} />
             <Scene key="home" component={Home} />
             <Scene key="tracking" component={Tracking} />
---
+++ App/pages/Guide.page.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'React'
+import {
+    StyleSheet, Text, View, TouchableHighlight,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import ScrollableTabView from 'react-native-scrollable-tab-view';
+import DefaultTabBar from './components/tabbar2.component';
+import NavBar, { NavButton, NavButtonText, NavTitle, NavGroup } from 'react-native-nav'
+import { Actions } from 'react-native-router-flux';
+import { WebView } from 'react-native-webview-messaging/WebView';
+
+import gilIntro from './webview/gil.introduce.html';
+import roadIntro from './webview/tour.introduce.html';
+import safetyIntro from './webview/safety.introduce.html';
+
+import navBarStylesModule from './assets/navbar.styles';
+const navBarStyles = navBarStylesModule("#568f4a");
+
+export default class Guide extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render(){
+        return (
+            <View style={styles.fill}>
+                <NavBar style={navBarStyles}>
+                    <View style={{flexDirection: 'row', marginLeft: -16}}>
+                        <NavButton style={{marginHorizontal: 14}} onPress={this.getMapCenter}>
+                            <Icon name="arrow-back" size={24} style={navBarStyles.backIcon} />
+                        </NavButton>
+                        <NavTitle style={navBarStyles.title}>
+                            이용 가이드
+                        </NavTitle>
+                    </View>
+                </NavBar>
+                <ScrollableTabView
+                    tabBarActiveTextColor='#568f4a'
+                    renderTabBar={()=> <DefaultTabBar />}
+                >
+                    <View tabLabel="소개" style={styles.fill}>
+                        <WebView source={gilIntro} />
+                    </View>
+                    <View tabLabel="둘레길" style={styles.fill}>
+                        <WebView source={roadIntro} />
+                    </View>
+                    <View tabLabel="안전수칙" style={styles.fill}>
+                        <WebView source={safetyIntro} />
+                    </View>
+                </ScrollableTabView>
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    fill: {
+        flex: 1,
+        backgroundColor: '#FFF',
+    }
+});
